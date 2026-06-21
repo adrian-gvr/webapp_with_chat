b69515eb599f6de4d23de93b0daca939ee00607f
Adrian <adrian82gavril@gmail.com>
2026-06-22 00:22:10 +0200

    Fix: added allowed Hosts and preview config for Render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,16 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     port: 5175,
     proxy: {
       "/api": "http://localhost:5001",
       "/uploads": "http://localhost:5001",
     },
+  },
+  preview: {
+    host: true,
+    port: 10000,
+    allowedHosts: ["webapp-with-chat-1.onrender.com"],
   },
 });
